e26dadc960bf7ec15fe1c0aa1c9b4692caa3447d
Joaquim Neto <joaquimmy@yahoo.com>
2021-01-16 11:43:01 -0300

    add setup methods

--- search/a-star/astar.v2.js
+++ search/a-star/astar.v2.js
@@ -1,7 +1,17 @@
 const PriorityQueue = require('priorityqueuejs');
 
+const GRID_SIZE = 16;
+const START = [0, 0];
+const END = [GRID_SIZE - 1, GRID_SIZE - 1];
+
 class Algorithm {
-  H(a, b) {
+  static run() {
+    const [grid, fScores, gScores] = Gridder.create(GRID_SIZE);
+    grid.get(Gridder.key(...START)).makeStart();
+    grid.get(Gridder.key(...END)).makeEnd();
+  }
+
+  static H(a, b) {
     // Manhattan distance
     return Math.abs(a.x - b.x) + Math.abs(a.y - b.y);
   }
@@ -13,6 +23,42 @@ class Point {
     this.y = y;
     this.neighbors = [];
     this.status = Point.statuses.EMPTY;
+  }
+
+  get key() {
+    return this.x + ' ' + this.y;
+  }
+
+  get isClosed() {
+    return this.status === Point.statuses.CLOSED;
+  }
+  get isOpen() {
+    return this.status === Point.statuses.OPEN;
+  }
+  get isBarrier() {
+    return this.status === Point.statuses.BARRIER;
+  }
+  get isStart() {
+    return this.status === Point.statuses.START;
+  }
+  get isEnd() {
+    return this.status === Point.statuses.END;
+  }
+
+  makeClosed() {
+    this.status = Point.statuses.CLOSED;
+  }
+  makeOpen() {
+    this.status = Point.statuses.OPEN;
+  }
+  makeBarrier() {
+    this.status = Point.statuses.BARRIER;
+  }
+  makeStart() {
+    this.status = Point.statuses.START;
+  }
+  makeEnd() {
+    this.status = Point.statuses.END;
   }
 
   static statuses = {
@@ -32,22 +78,41 @@ class Gridder {
 
   static create(size) {
     const grid = new Map();
+    const fScores = new Map();
+    const gSCores = new Map();
+
     for (let row = 0; row < size; row++) {
       for (let col = 0; col < size; col++) {
-        grid.set(row + ' ' + col, new Point(x, y));
+        grid.set(row + ' ' + col, new Point(row, col));
+        fScores.set(row + ' ' + col, Infinity);
+        gSCores.set(row + ' ' + col, Infinity);
       }
     }
-    return grid;
+    Gridder.updateNeighbors(grid);
+    return [grid, fScores, gSCores];
   }
 
-  static infinity(size) {
-    const grid = new Map();
-    for (let row = 0; row < size; row++) {
-      for (let col = 0; col < size; col++) {
-        grid.set(row + ' ' + col, Infinity);
+  static updateNeighbors(grid) {
+    for (let x = 0; x < GRID_SIZE; x++) {
+      for (let y = 0; y < GRID_SIZE; y++) {
+        // down
+        if (row < GRID_SIZE - 1 && !grid.get(Gridder.key(row + 1, col)).isBarrier) {
+          grid.get(Gridder.key(row, col)).neighbors.push(grid.get(Gridder.key(row + 1, col)));
+        }
+        // up
+        if (row > 0 && !grid.get(Gridder.key(row - 1, col)).isBarrier) {
+          grid.get(Gridder.key(row, col)).neighbors.push(grid.get(Gridder.key(row - 1, col)));
+        }
+        // right
+        if (col < GRID_SIZE - 1 && !grid.get(Gridder.key(row, col + 1)).isBarrier) {
+          grid.get(Gridder.key(row, col)).neighbors.push(grid.get(Gridder.key(row, col + 1)));
+        }
+        // left
+        if (col > 0 && !grid.get(Gridder.key(row, col - 1)).isBarrier) {
+          grid.get(Gridder.key(row, col)).neighbors.push(grid.get(Gridder.key(row, col - 1)));
+        }
       }
     }
-    return grid;
   }
 
   // static display(grid) {
